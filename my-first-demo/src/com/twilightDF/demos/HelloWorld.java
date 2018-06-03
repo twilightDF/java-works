a748450651bc29e7439166dbd47c915c042668ff
twilightDF <1159990340@qq.com>
2018-06-03 17:22:45 +0800

    init

---
+++ my-first-demo/src/com/twilightDF/demos/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.twilightDF.demos;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		String name ="dufeng";
+		System.out.println("hello world " + name);
+		
+	}
+
+}
