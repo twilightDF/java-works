4448513b9f78690a32ca35eda24cbc9db4f3a522
twilightDF <1159990340@qq.com>
2018-08-19 22:13:10 +0800

    测试scanner获得键盘输入

---
+++ my-first-demo/src/com/twilightDF/demos/TextScanner.java
@@ -0,0 +1,26 @@
+package com.twilightDF.demos;
+import java.util.Scanner;
+/**
+ * 测试用sanner获取键盘输入
+ * @author DELL
+ *
+ */
+public class TextScanner {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("请输入你的名字：");	
+		String name = scanner.nextLine();
+		System.out.println("请输入你的年龄：");
+		int age = scanner.nextInt();
+		System.out .println("***************************");
+		
+		System.out.println(name);
+		System.out.println("已经来到地球的天数：" + age * 365);
+		System.out.println("在地球剩余的天数：" + (72-age) * 365);
+		
+		System.out .println("珍惜时间，努力学习！");
+	}
+	
+}
