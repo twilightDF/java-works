e3994d9ac5c6afcab756a0bc6598ab1fd6ea86c7
twilightDF <1159990340@qq.com>
2018-08-24 21:48:58 +0800

    方法的基本运用

---
+++ my-first-demo/src/com/twilightDF/demos/TextMethod.java
@@ -0,0 +1,33 @@
+package com.twilightDF.demos;
+/**
+ * 方法的基本运用
+ * @author 杜锋
+ *
+ */
+public class TextMethod {
+
+	public static void main(String[] args) {
+		TextMethod methodUse = new TextMethod();//new一个对象，方法不能直接调用，必须通过对象调用!
+		methodUse.print();
+		System.out.println("#########################################");
+		int c = 100 + methodUse.add(12, 13, 14);//实参：运用方法时输入的参数。
+												//调用出来的add方法可直接作为一个int类型的数进行运算！
+		System.out.println(methodUse.add(12, 13, 14));
+		System.out.println(c);
+		
+	} 
+	/**
+	 * 不含参数的方法
+	 */
+	void/*void表示无返回值！*/ print() {
+		System.out.println("这句话是调用的print方法！");
+	}
+	/**
+	 * 放参数的方法
+	 */
+	int/*返回值为int类型*/ add(int a,int b,int c)/*形参：在编写方法时使用*/ {
+		int sum = a + b + c;
+		return sum;//return有两个作用。1.结束方法的运行。2.返回值。
+	}
+		
+}
