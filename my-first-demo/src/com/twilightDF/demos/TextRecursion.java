32ea5d9bfa97f2dad11494370aa41b0e4f63c063
twilightDF <1159990340@qq.com>
2018-08-26 10:29:48 +0800

    测试递归

---
+++ my-first-demo/src/com/twilightDF/demos/TextRecursion.java
@@ -0,0 +1,53 @@
+package com.twilightDF.demos;
+
+import com.sun.jndi.url.iiopname.iiopnameURLContextFactory;
+
+/**
+ * 测试递归功能
+ * @author 杜锋
+ *
+ */
+public class TextRecursion {
+
+	public static void main(String[] arg) {
+		//该功能实现10的递乘！
+		TextRecursion fa = new TextRecursion();
+		System.out.println("用递归算的10！ ：" + fa.factoriaR(10));
+		
+		System.out.println("用循环算的10！ ：" + fa.fatcoriaL(10));
+
+	}
+	
+	int factoriaR(int n) {
+
+		if(n == 0) {
+			return 1;
+		}else {
+			return n * factoriaR(n - 1);
+		}
+	}
+	int fatcoriaL(int n) {
+
+		int a = 1;
+		while (n > 1) {
+			a *= n;
+			n -= 1;
+		}
+		return a;
+		
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+
