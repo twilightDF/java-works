daa3105d8c63aef5b14090f48c0468e197c280a7
twilightDF <1159990340@qq.com>
2018-08-30 18:30:48 +0800

    加入输入年龄范围限制

--- JustTestEncapsulation/src/com/twilight/demos/theRuleOfEncapsulation.java
+++ JustTestEncapsulation/src/com/twilight/demos/theRuleOfEncapsulation.java
@@ -8,10 +8,10 @@ public class theRuleOfEncapsulation {
 	public static void main(String[] args) {
 
 		Person p = new Person();
-		p.getName("杜锋");
-		p.getAge(20);
-		System.out.println(p.setName());
-		System.out.println(p.setAge());
+		p.setName("杜锋");
+		p.setAge(20);
+		System.out.println(p.getName());
+		System.out.println(p.getAge());
 	}
 
 }
@@ -19,31 +19,52 @@ public class theRuleOfEncapsulation {
 class Person{
 
 	//一般来说类的属性都私有，只暴露其方法
-	 private int age;
+	private int age;
 	private String name;
 	private int height;
 	private String address;
 	
 	//如果其他类需要调用本类的方法，使用get或set方法
-	public void getName(String name) {
+	//可以使用系统快捷shift + alt + s自动构造
+	public int getAge() {
+		return age;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setAge(int age) {
+
+		if(age <= 0||age >= 140) {
+			System.out.println("请输入正常的年龄！");
+		}else {
+			this.age = age;
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
 		this.name = name;
 	}
 	
-	public String setName() {
-		return name;
-	}
-	public void getAge(int age) {
-		this.age = age;
-	}
-	public int setAge() {
-		return age;
-	}
 	
 	
 	
-	
-	
-	
-	
-	//
+
 }
